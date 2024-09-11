7ee0a65e54ca8ed0495cae26fef66b323cf62746
Doug Hirlinger <doughirlinger@gmail.com>
2024-09-11 10:39:57 -0400

    complete

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+    private String courseName;
+    private String instructor;
+    private ArrayList<Student> students;
+}
+
+
--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -7,4 +7,41 @@ public class Student {
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+        public Student(String name, int studentId, int numberOfCredits, double gpa){
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
+        public String getName(){
+                return name;
+        }
+
+        public void setName(String aName){
+                this.name = aName;
+        }
+
+        public int getStudentId(){
+                return studentId;
+        }
+
+       private void setStudentId(int aStudentId){
+                this.studentId = aStudentId;
+       }
+
+       public int getNumberOfCredits(){
+                return numberOfCredits;
+       }
+
+       private void setNumberOfCredits(int aNumberOfCredits){
+                this.numberOfCredits = aNumberOfCredits;
+       }
+
+       public double getGpa() {
+               return gpa;
+       }
+
+       private void setGpa(double aGpa) {
+               this.gpa = aGpa;
+       }
 }
